8bcc4cfb8d45d6c75fdc308c4cc4f505fa1ff8f8
Aubrey Crook <aubreycrook@hotmail.com>
2022-01-29 18:25:33 -0500

    Done the skeleton for the server application

---
+++ server/server.js
@@ -0,0 +1,89 @@
+const inquirer =  require("inquirer")
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+async function viewalldepartments(){
+
+}
+
+async function viewallemployees(){
+    
+}
+
+async function viewallroles(){
+    
+}
+
+async function addanemployee(){n
+    
+}
+
+async function addanrole(){
+
+
+    
+}
+
+async function addandepartment(){
+
+    
+    
+}
+async function updateanemployeerole(){
+
+    
+    
+}
+
+async function showmainmenu(){
+    let choice = await inquirer.prompt({
+    type: "list",
+    name: "choice",
+    message:"What would you like to do?",
+    choices: [
+        "view all departments",
+        "view all employees",
+        "view all roles",
+        "add an employee",
+        "add an deparment",
+        "add an role",
+        "update an employee role",
+    ]
+})
+switch (choice.choice){
+    case "view all departments":
+        viewalldepartments()
+        break;
+    case "view all employees":
+        viewallemployees()
+        break
+    case "view all roles":
+        viewallroles()
+        break
+    case "add an employee":
+        addanemployee()
+        break
+    case "add an deparment":
+        addandepartment()
+        break
+    case "add an role":
+        addanrole()
+        break
+    case "update an employee role":
+        updateanemployeerole()
+        break
+        default:
+            console.log("invalid selection")
+            showmainmenu()
+}
+
+
+    
+    
+}
